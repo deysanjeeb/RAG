24c3c19c2730922762e0600cde6b587d0459b171
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-06-10 00:24:28 +0530

    create streamlit frontend

--- main.py
+++ main.py
@@ -10,61 +10,35 @@ import chromadb
 import numpy as np
 import os
 import pathlib
-
-
+import streamlit as st
+import datetime
 
 # creating a pdf reader object 
 reader = PdfReader(os.getcwd()+"/pdfs/policy-booklet-0923.pdf") 
   
 # printing number of pages in pdf file 
 print(len(reader.pages)) 
-  
-# creating a page object 
-# for i in range(2,len(reader.pages)):
-#     page = reader.pages[i] 
-  
-#     text = page.extract_text()
-#     print(text)
-   
-#     text = text.replace('\n',' ').split('?')
-#     print((text))
-#     text_embeddings = np.array([get_text_embedding(chunk) for chunk in text])
-
-
-page = reader.pages[2] 
-  
-text = page.extract_text()
-print(text)
-
-text = text.replace('\n',' ').split('?')
-print((text))
 client = chromadb.PersistentClient(os.getcwd())
+# collection = client.create_collection(name="docs")
+collection = client.get_collection(name='docs')
+# creating a page object 
+# if not os.path.exists(os.getcwd()+'\chroma.sqlite3'):
+#     for j in range(2,len(reader.pages)):
+#         page = reader.pages[j] 
+#         text = page.extract_text()
+#         print(text)
+#         text = text.replace('\n',' ').split('?')
+#         for i, d in enumerate(text):
+#             response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+#             embedding = response["embedding"]
+#             collection.add(
+#                 ids=[str(i)],
+#                 embeddings=[embedding],
+#                 documents=[d]
+#         )
 
-if not os.path.exists(os.get_cwd()+'chroma.sqlite3'):
-    collection = client.create_collection(name="docs")
-else:
-    collection = client.get_collection()
-# store each document in a vector embedding database
-for i, d in enumerate(text):
-    response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
-    embedding = response["embedding"]
-    collection.add(
-        ids=[str(i)],
-        embeddings=[embedding],
-        documents=[d]
-    )   
-   # questions= []
-#     answers = []
-#     answer=''
-#     for line in text:
-#         if line[-1] == '?':
-#             questions.append(line)
-#             print(line)
-#             print(answer)
-#             answers.append(answer)
-#             answer = ''
-#         else:
-#             answer = answer + line
-        
-# print(len(questions))
-# print(len(answers))
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
