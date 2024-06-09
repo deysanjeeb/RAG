e34c57a0d1381b03c694e77ef0c8e241b5f2eda5
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-06-09 16:51:36 +0530

    create embeddings

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
 from pypdf import PdfReader 
-  
+from groq import Groq
+import json
+import os
+from io import BytesIO
+import re
+
+import ollama
+import chromadb
+import numpy as np
+import os
+
+
+
 # creating a pdf reader object 
-reader = PdfReader('/home/skd/Downloads/SSRN-id3614509(1).pdf') 
+reader = PdfReader(os.getcwd()+"/pdfs/policy-booklet-0923.pdf") 
   
 # printing number of pages in pdf file 
 print(len(reader.pages)) 
   
 # creating a page object 
-page = reader.pages[0] 
+# for i in range(2,len(reader.pages)):
+#     page = reader.pages[i] 
   
-# extracting text from page 
-print(page.extract_text()) 
+#     text = page.extract_text()
+#     print(text)
+   
+#     text = text.replace('\n',' ').split('?')
+#     print((text))
+#     text_embeddings = np.array([get_text_embedding(chunk) for chunk in text])
+
+
+page = reader.pages[2] 
+  
+text = page.extract_text()
+print(text)
+
+text = text.replace('\n',' ').split('?')
+print((text))
+client = chromadb.Client()
+collection = client.create_collection(name="docs")
+
+# store each document in a vector embedding database
+for i, d in enumerate(text):
+    response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+    embedding = response["embedding"]
+    collection.add(
+        ids=[str(i)],
+        embeddings=[embedding],
+        documents=[d]
+    )   
+   # questions= []
+#     answers = []
+#     answer=''
+#     for line in text:
+#         if line[-1] == '?':
+#             questions.append(line)
+#             print(line)
+#             print(answer)
+#             answers.append(answer)
+#             answer = ''
+#         else:
+#             answer = answer + line
+        
+# print(len(questions))
+# print(len(answers))
